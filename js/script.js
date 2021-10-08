192078dcbd5dc67c85a09fff7e46ed95a6068f81
flashbLack98 <npalakuttige98@icloud.com>
2021-10-08 16:34:49 +0200

    complete (with isNaN but without Parseint)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 
-const km = 150;
+const km = prompt("Quanti km desideri fare?");
 console.log("n di kilometri " + km);
 
+if (isNaN(km)) {
+    alert("I Km inseriti non sono validi. Utilizza solo numeri ");
+}
 
 const etaPasseggero = prompt("Quanti anni hai?");
 console.log("età " + etaPasseggero);
+
+if (isNaN(etaPasseggero)) {
+    alert("La tua età non è valida. Inseriscila utilizzando soltanto numeri");
+}
 
 
 let costoBiglietto = km * 0.21;
@@ -19,13 +26,24 @@ let bigliettoScontato40 = (costoBiglietto) - ((costoBiglietto / 100) * 40);
 bigliettoScontato40 = bigliettoScontato40.toFixed(2);
 
 
+
 if (etaPasseggero < 18) {
-    alert("Hai diritto di uno sconto del 20% perchè sei minorenne");
+    alert("Complimenti! Hai diritto ad uno sconto del 20% perchè sei minorenne");
     costoBiglietto = bigliettoScontato20;
-    console.log("il tuo biglietto scontato costa " + bigliettoScontato20);
+    console.log("il tuo biglietto, scontato al 20%, costa " + bigliettoScontato20);
 } else if (etaPasseggero >= 65) {
+    alert("Complimenti! Hai diritto ad uno sconto del 40% perchè hai più di 65 anni");
     costoBiglietto = bigliettoScontato40;
-    console.log("il tuo biglietto scontato costa " + bigliettoScontato40);
+    console.log("il tuo biglietto, scontato al 40%, costa " + bigliettoScontato40);
 }
 
-alert("il tuo biglietto costa " + costoBiglietto + "€");
+if (isNaN(costoBiglietto)) {
+    alert("Hai inserito scorrettamente i km");
+} else {
+    alert("il tuo biglietto costa " + costoBiglietto + "€");
+}
+
+
+
+
+
