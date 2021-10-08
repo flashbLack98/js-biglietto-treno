7a9e090a48e7504bb585a0998752131f87decc11
flashbLack98 <npalakuttige98@icloud.com>
2021-10-08 18:20:36 +0200

    COMPLETE

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-
+//KM
 const km = parseInt(prompt("Quanti km desideri fare?"));
 console.log("n di kilometri " + km);
 
+//KM NON VALIDI
 if (isNaN(km)) {
     alert("I Km inseriti non sono validi. Utilizza solo numeri ");
 }
 
+//ETA'
 const etaPasseggero = parseInt(prompt("Quanti anni hai?"));
 console.log("età " + etaPasseggero);
 
+//ETA' NON VALIDA
 if (isNaN(etaPasseggero)) {
     alert("La tua età non è valida. Inseriscila utilizzando soltanto numeri");
 }
@@ -24,8 +27,7 @@ bigliettoScontato20 = bigliettoScontato20.toFixed(2);
 let bigliettoScontato40 = (costoBiglietto) - ((costoBiglietto / 100) * 40);
 bigliettoScontato40 = bigliettoScontato40.toFixed(2);
 
-
-
+//VARIABILE ETA
 if (etaPasseggero < 18) {
     alert("Complimenti! Hai diritto ad uno sconto del 20% perchè sei minorenne");
     costoBiglietto = bigliettoScontato20;
@@ -36,11 +38,21 @@ if (etaPasseggero < 18) {
     console.log("il tuo biglietto, scontato al 40%, costa " + bigliettoScontato40);
 }
 
+
+//VARIABILE PREZZO FINALE
 if (isNaN(costoBiglietto)) {
     alert("Hai inserito scorrettamente i km");
+
 } else {
     alert("il tuo biglietto costa " + costoBiglietto + "€");
 }
+
+if (isNaN(etaPasseggero)) {
+    alert("Hai inserito scorrettamente la tua età");
+}
+
+
+
 
 
 
